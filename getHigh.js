a8241be03db390fcf81ac3b86242795a10dc08e2
Kevin Margatan <kmargatan@gatech.edu>
2019-05-22 16:51:52 -0400

    Fixed getHigh function decalaration

--- getHigh.js
+++ getHigh.js
@@ -1,9 +1,9 @@
-zdUrl+"function getHigh(sheetURL, timeNow, highAged) {
+function getHigh(sheetURL, timeNow, highAged) {
   var highTable = [];
 
   //HIGH
   for (var i=0; i<highAged[0].length; i++) {
-    var parse = getAPIdata(zdUrl+"/api/v2/"+highAged[0][i]+".json", zdAuth);
+    var parse = getAPIdata("https://airwatch.zendesk.com/api/v2/"+highAged[0][i]+".json", zdAuth);
 
 
   }
