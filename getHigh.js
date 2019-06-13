0b2b88f916596abe822992c78c6ad5d38a774dc4
Kevin Margatan <kmargatan@gatech.edu>
2019-06-13 10:00:22 -0400

    Updated to use reference Zendesk URL

--- getHigh.js
+++ getHigh.js
@@ -3,7 +3,7 @@ function getHigh(sheetURL, timeNow, highAged) {
 
   //HIGH
   for (var i=0; i<highAged[0].length; i++) {
-    var parse = getAPIdata("https://airwatch.zendesk.com/api/v2/"+highAged[0][i]+".json", zdAuth);
+    var parse = getAPIdata(zdUrl+"api/v2/"+highAged[0][i]+".json", zdAuth);
 
 
   }
