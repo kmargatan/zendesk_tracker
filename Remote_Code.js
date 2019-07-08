c4f8872b7bc7b92657e14bfbcb894ba51c03ec6f
Kevin Margatan <50299075+kmargatan@users.noreply.github.com>
2019-07-08 16:01:54 -0400

    Adding function initSheet for one-touch deployment

--- Remote_Code.js
+++ Remote_Code.js
@@ -69,3 +69,31 @@ function sandBox() {
   Logger.log(master.getSheetByName('Attn').getRange('B26').getValue());
 
 }
+
+function initSheet() {
+  var firstStart = false;
+  var firstSheet = master.getActiveSheet();
+  
+  if (master.getSheetByName("Main") == null) {
+    master.insertSheet("Main");
+    firstStart = true;
+  }
+  
+  if (master.getSheetByName("OOO") == null) {
+    master.insertSheet("OOO");
+    firstStart = true;
+  }
+  
+  if (master.getSheetByName("Attn") == null) {
+    master.insertSheet("Attn");
+    firstStart = true;
+  }
+  
+  if (firstStart) {
+    master.deleteSheet(firstSheet);
+    master.getSheetByName("Main").hideColumns(2,3);
+    master.getSheetByName("OOO").hideColumns(2,3);
+  }
+  
+  getUsers();
+}
